e457390e8d3c48b02a96fab765bc0c5756b3ebc7
yjson <handyejee14@gmail.com>
2024-12-04 06:01:25 +0900

    fix: product client 파일 수정

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/client/ProductClient.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/client/ProductClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", fallback = ProductClient.class) // fallback 로직 실행
+@FeignClient(name = "product-service", fallback = PublicClientFallback.class) // fallback 로직 실행
 public interface ProductClient {
 
   @GetMapping("/product/{productId}")
